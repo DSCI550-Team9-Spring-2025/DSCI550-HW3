9d2c00f69196c9dbbe6067cee25c3c57c40e0527
Dark-eXe <andrewtur510@gmail.com>
2025-04-12 00:50:19 -0700

    Reformatting

--- Source/react-ui/src/App.js
+++ Source/react-ui/src/App.js
@@ -4,6 +4,10 @@ import WordCloud from './components/WordCloud';
 function App() {
   return (
     <div className="App">
+      <div className="title-wrapper">
+        <h1>Haunted Places in the US</h1>
+        <h2>Visualized with D3</h2>
+      </div>
       <WordCloud />
     </div>
   );
